f22290d1720aae690673c8fd190094dac5641743
cmudduluru <chandrika.mudduluru@actian.com>
2021-08-06 13:14:53 -0500

    Initial commit

---
+++ src/main/java/com/example/java8projectupdatetojava11/Java8projectupdatetojava11Application.java
@@ -0,0 +1,13 @@
+package com.example.java8projectupdatetojava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8projectupdatetojava11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8projectupdatetojava11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java8projectupdatetojava11/Java8projectupdatetojava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8projectupdatetojava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8projectupdatetojava11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
